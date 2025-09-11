6310f8a9b4de407b643e95997672299748cf9524
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-09-11 04:48:32 +0300

    init match chips of choices

--- app/src/main/java/com/karrar/movieapp/ui/match/matchChoices/MatchChoicesFragment.kt
+++ app/src/main/java/com/karrar/movieapp/ui/match/matchChoices/MatchChoicesFragment.kt
@@ -7,6 +7,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.chip.ChipGroup
 import com.karrar.movieapp.R
 import com.karrar.movieapp.databinding.FragmentMatchChoicesBinding
 import com.karrar.movieapp.ui.base.BaseFragment
@@ -20,7 +21,82 @@ class MatchChoicesFragment : BaseFragment<FragmentMatchChoicesBinding>() {
         setTitle(false)
         initButton()
         initBackAction()
+        initChipsMood()
+        initChipsGenre()
+        initChipTime()
+        initAgeTime()
         init()
+
+    }
+
+    private fun initAgeTime() {
+        binding.recent.setOnClickListener {
+            binding.recent.isSelected = true
+            binding.classic.isSelected = false
+            binding.both.isSelected = false
+            makeButtonClickable()
+        }
+        binding.classic.setOnClickListener {
+            binding.recent.isSelected = false
+            binding.classic.isSelected = true
+            binding.both.isSelected = false
+            makeButtonClickable()
+        }
+        binding.both.setOnClickListener {
+            binding.recent.isSelected = false
+            binding.classic.isSelected = false
+            binding.both.isSelected = true
+            makeButtonClickable()
+        }
+    }
+
+
+    private fun initChipTime() {
+        for (i in 0 until binding.timeGroup.childCount) {
+            val chip = binding.timeGroup.getChildAt(i)
+            chip.setOnClickListener {
+                makeSelectFromTime(index = i)
+                makeButtonClickable()
+            }
+        }
+    }
+
+
+    private fun makeSelectFromTime(index : Int ){
+        for (i in 0 until binding.timeGroup.childCount) {
+            val chip = binding.timeGroup.getChildAt(i)
+            chip.isSelected = i == index
+        }
+    }
+
+    private fun initChipsGenre() {
+        for (i in 0 until binding.genreGroup.childCount) {
+            val chip = binding.genreGroup.getChildAt(i)
+            chip.setOnClickListener {
+                chip.isSelected = !chip.isSelected
+                manageButtonClickable(binding.genreGroup)
+            }
+        }    }
+
+    private fun initChipsMood() {
+        for (i in 0 until binding.moodGroup.childCount) {
+            val chip = binding.moodGroup.getChildAt(i)
+            chip.setOnClickListener {
+                chip.isSelected = !chip.isSelected
+                manageButtonClickable(binding.moodGroup)
+            }
+        }
+    }
+
+    private fun manageButtonClickable(chipGroup : ChipGroup){
+        for(i in 0 until chipGroup.childCount ){
+            val child = chipGroup.getChildAt(i)
+            if(child.isSelected){
+                makeButtonClickable()
+                return
+            }
+        }
+        makeButtonNotClickable()
     }
 
 
@@ -35,7 +111,31 @@ class MatchChoicesFragment : BaseFragment<FragmentMatchChoicesBinding>() {
         }
     }
 
+    private fun disAppearAllChipsNotSelected(chipGroup : ChipGroup){
+        for(i in 0 until chipGroup.childCount ){
+            val child = chipGroup.getChildAt(i)
+            if(!child.isSelected){
+                child.visibility  = View.GONE
+            }else{
+                child.alpha = 0.30f
+            }
+        }
+    }
+    private fun appearAllChipsNotSelected(chipGroup : ChipGroup){
+        for(i in 0 until chipGroup.childCount ){
+            val child = chipGroup.getChildAt(i)
+            child.visibility  = View.VISIBLE
+            child.alpha = 1f
 
+        }
+    }
+    private fun makeAllSelectedFalse(chipGroup : ChipGroup){
+        for(i in 0 until chipGroup.childCount ){
+            val child = chipGroup.getChildAt(i)
+            child.isSelected = false
+
+        }
+    }
     private fun initNextButtonAction() {
         binding.startMatchingButton.setOnClickListener {
             when (counter) {
@@ -43,12 +143,28 @@ class MatchChoicesFragment : BaseFragment<FragmentMatchChoicesBinding>() {
                     counter++
                     makeButtonNotClickable()
                     handleLoadingColorLocation(binding.second.id)
+
+                    disAppearAllChipsNotSelected(binding.moodGroup)
+                    binding.moodTitle.alpha = 0.30f
+                    binding.genreTitle.visibility = View.VISIBLE
+                    binding.genreGroup.visibility = View.VISIBLE
+
+                    makeAllSelectedFalse(binding.genreGroup)
+
                 }
 
                 1 -> {
                     counter++
                     makeButtonNotClickable()
                     handleLoadingColorLocation(binding.third.id)
+
+                    disAppearAllChipsNotSelected(binding.genreGroup)
+                    binding.genreTitle.alpha = 0.30f
+                    binding.timeGroup.visibility = View.VISIBLE
+                    binding.timeTitle.visibility = View.VISIBLE
+
+                    makeAllSelectedFalse(binding.timeGroup)
+
                 }
 
                 2 -> {
@@ -56,9 +172,35 @@ class MatchChoicesFragment : BaseFragment<FragmentMatchChoicesBinding>() {
                     makeButtonNotClickable()
                     handleLoadingColorLocation(binding.loader.id)
                     changeButtonText(getString(R.string.start_matching))
+
+                    disAppearAllChipsNotSelected(binding.timeGroup)
+                    binding.timeTitle.alpha = 0.30f
+                    binding.ageTitle.visibility = View.VISIBLE
+                    binding.ageGroup.visibility = View.VISIBLE
+
+
+                    binding.recent.isSelected = false
+                    binding.classic.isSelected = false
+                    binding.both.isSelected = false
                 }
 
                 3 -> {
+                    counter++
+                    binding.ageTitle.alpha = 0.3f
+                    if (binding.recent.isSelected){
+                        binding.classic.visibility = View.GONE
+                        binding.both.visibility = View.GONE
+                        binding.recent.alpha = 0.30f
+
+                    }else if ( binding.classic.isSelected){
+                        binding.classic.alpha = 0.30f
+                        binding.both.visibility = View.GONE
+                        binding.recent.visibility = View.GONE
+                    }else{
+                        binding.classic.visibility = View.GONE
+                        binding.both.alpha =0.30f
+                        binding.recent.visibility = View.GONE
+                    }
 
                 }
             }
@@ -76,12 +218,21 @@ class MatchChoicesFragment : BaseFragment<FragmentMatchChoicesBinding>() {
                     counter--
                     handleLoadingColorLocation(binding.first.id)
                     makeButtonClickable()
+                    appearAllChipsNotSelected(binding.moodGroup)
+                    binding.moodTitle.alpha = 1f
+                    binding.genreGroup.visibility = View.GONE
+                    binding.genreTitle.visibility = View.GONE
                 }
 
                 2 -> {
                     counter--
                     handleLoadingColorLocation(binding.second.id)
                     makeButtonClickable()
+
+                    appearAllChipsNotSelected(binding.genreGroup)
+                    binding.genreTitle.alpha = 1f
+                    binding.timeGroup.visibility = View.GONE
+                    binding.timeTitle.visibility = View.GONE
                 }
 
                 3 -> {
@@ -89,6 +240,23 @@ class MatchChoicesFragment : BaseFragment<FragmentMatchChoicesBinding>() {
                     handleLoadingColorLocation(binding.third.id)
                     makeButtonClickable()
                     changeButtonText(getString(R.string.next))
+
+                    appearAllChipsNotSelected(binding.timeGroup)
+                    binding.timeTitle.alpha = 1f
+                    binding.ageTitle.visibility = View.GONE
+                    binding.ageGroup.visibility = View.GONE
+                }
+                4->{
+                    counter--
+                        binding.ageTitle.alpha = 1f
+                        binding.classic.visibility = View.VISIBLE
+                        binding.both.visibility = View.VISIBLE
+                        binding.recent.visibility = View.VISIBLE
+
+                        binding.recent.alpha = 1f
+                        binding.classic.alpha = 1f
+                        binding.both.alpha = 1f
+
                 }
             }
         }

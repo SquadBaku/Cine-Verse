0f024b7159c66ee9f3bf68be281d8a2be08ff6a7
Alaa <alaakhaled9191@gmail.com>
2025-09-11 22:29:54 +0300

    add fragment to match result pager

---
+++ app/src/main/java/com/karrar/movieapp/ui/match/matchPager/MatchPagerViewModel.kt
@@ -0,0 +1,15 @@
+package com.karrar.movieapp.ui.match.matchPager
+
+import com.karrar.movieapp.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MatchPagerViewModel @Inject constructor() : BaseViewModel() {
+
+
+    override fun getData() {
+
+    }
+
+}

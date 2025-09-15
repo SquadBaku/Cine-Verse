af427162f2df84901deb989a9b97353a9aaf27f4
saraashraf11 <saraashraf1822004@gmail.com>
2025-09-15 21:30:54 +0300

    feature: add onClickSeeAllCollections in HomeInteractionListener

--- app/src/main/java/com/karrar/movieapp/ui/home/HomeInteractionListener.kt
+++ app/src/main/java/com/karrar/movieapp/ui/home/HomeInteractionListener.kt
@@ -5,4 +5,5 @@ import com.karrar.movieapp.ui.base.BaseInteractionListener
 interface HomeInteractionListener : BaseInteractionListener {
     fun onClickSeeAllActors()
     fun onClickSeeAllRecentlyViewed()
+    fun onClickSeeAllCollections()
 }

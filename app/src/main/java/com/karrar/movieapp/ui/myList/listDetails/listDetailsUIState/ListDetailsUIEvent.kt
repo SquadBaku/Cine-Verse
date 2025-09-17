26f3810ea62fb61e4359208821aafce429150a48
saraashraf11 <saraashraf1822004@gmail.com>
2025-09-18 01:09:27 +0300

    refactor: update ListDetailsUIEvent to add ShowMessage

--- app/src/main/java/com/karrar/movieapp/ui/myList/listDetails/listDetailsUIState/ListDetailsUIEvent.kt
+++ app/src/main/java/com/karrar/movieapp/ui/myList/listDetails/listDetailsUIState/ListDetailsUIEvent.kt
@@ -2,4 +2,5 @@ package com.karrar.movieapp.ui.myList.listDetails.listDetailsUIState
 
 sealed interface ListDetailsUIEvent {
     data class OnItemSelected(val savedMediaUIState: SavedMediaUIState) : ListDetailsUIEvent
+    data class ShowMessage(val message: String) : ListDetailsUIEvent
 }

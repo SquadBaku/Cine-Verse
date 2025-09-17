d18daa86954fc0a6bca0e2435b6026076dcc4acb
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-09-17 18:22:42 +0300

    refactor titles of section

--- app/src/main/java/com/karrar/movieapp/domain/enums/HomeItemsType.kt
+++ app/src/main/java/com/karrar/movieapp/domain/enums/HomeItemsType.kt
@@ -3,13 +3,13 @@ package com.karrar.movieapp.domain.enums
 enum class HomeItemsType(val value :String) {
     ON_THE_AIR("On the air"),
     TRENDING("Trending"),
-    NOW_STREAMING("Now streaming"),
-    UPCOMING("Upcoming"),
+    NOW_STREAMING("Recently Released"),
+    UPCOMING("Upcoming Movies"),
     MYSTERY("Mystery"),
     ADVENTURE("Adventure"),
-    RECENTLY_VIEWED("Recently viewed"),
+    RECENTLY_VIEWED("You Recently Viewed"),
     COLLECTIONS("Your Collections"),
-    TOP_RATED_MOVIE("Top Rated Movie"),
-    MATCH_YOUR_VIBE("Match Your Vibe"),
+    TOP_RATED_MOVIE("Top Rated TV Shows"),
+    MATCH_YOUR_VIBE("Matches your Vibe"),
     NON("")
 }
--- app/src/main/java/com/karrar/movieapp/ui/allMedia/AllMovieFragment.kt
+++ app/src/main/java/com/karrar/movieapp/ui/allMedia/AllMovieFragment.kt
@@ -93,12 +93,12 @@ class AllMovieFragment : BaseFragment<FragmentAllMovieBinding>() {
             AllMediaType.POPULAR -> resources.getString(R.string.popular)
             AllMediaType.TOP_RATED -> resources.getString(R.string.top_rated_movie)
             AllMediaType.TRENDING -> resources.getString(R.string.title_trending)
-            AllMediaType.NOW_STREAMING -> resources.getString(R.string.title_streaming)
+            AllMediaType.NOW_STREAMING -> resources.getString(R.string.title_recently_released)
             AllMediaType.UPCOMING -> resources.getString(R.string.title_upcoming)
             AllMediaType.MYSTERY -> resources.getString(R.string.title_mystery)
             AllMediaType.ADVENTURE -> resources.getString(R.string.title_adventure)
             AllMediaType.ACTOR_MOVIES -> ""
-            AllMediaType.MATCH_YOUR_VIBE -> resources.getString(R.string.match_your_vibe)
+            AllMediaType.MATCH_YOUR_VIBE -> resources.getString(R.string.matches_your_vibe)
         }
     }
 

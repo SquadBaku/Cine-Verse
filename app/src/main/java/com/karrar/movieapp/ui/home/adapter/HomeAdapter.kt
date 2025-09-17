19d6988d449fa3d9b455da6d560d1c2710493f4d
Alaa <alaakhaled9191@gmail.com>
2025-09-17 18:49:40 +0300

    add need more to watch item in home adapter

--- app/src/main/java/com/karrar/movieapp/ui/home/adapter/HomeAdapter.kt
+++ app/src/main/java/com/karrar/movieapp/ui/home/adapter/HomeAdapter.kt
@@ -158,6 +158,10 @@ class HomeAdapter(
                 is HomeItem.WhatShouldIWatch -> {
                     holder.binding.setVariable(BR.listener, listener as HomeInteractionListener)
                 }
+
+                is HomeItem.NeedMoreToWatch -> {
+                    holder.binding.setVariable(BR.listener, listener as HomeInteractionListener)
+                }
             }
     }
 
@@ -196,6 +200,7 @@ class HomeAdapter(
                 is HomeItem.AiringToday -> R.layout.list_airing_today
                 is HomeItem.OnTheAiring -> R.layout.list_tvshow
                 is HomeItem.WhatShouldIWatch -> R.layout.what_should_i_watch
+                is HomeItem.NeedMoreToWatch -> R.layout.need_more_to_watch
                 is HomeItem.RecentlyViewed -> R.layout.list_recently_viewed
                 is HomeItem.Adventure,
                 is HomeItem.Mystery,

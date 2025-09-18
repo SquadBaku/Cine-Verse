462bd7e777c9a1ee7bd24e63f28e35a1a8e9a432
kareem esam <kareem.eldeen.esam@gmail.com>
2025-09-18 04:48:26 +0300

    feat: add SingleActionListener interface and new strings for enhanced UI interactions

---
+++ app/src/main/java/com/karrar/movieapp/utilities/SingleActionListener.kt
@@ -0,0 +1,7 @@
+package com.karrar.movieapp.utilities
+
+import com.karrar.movieapp.ui.base.BaseInteractionListener
+
+interface SingleActionListener : BaseInteractionListener {
+    fun onClick()
+}

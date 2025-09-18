095fa4ab1055da589a52e54b3c12186d875efb8b
kareem esam <kareem.eldeen.esam@gmail.com>
2025-09-18 04:45:45 +0300

    feat: add character name to actor model and UI state

--- app/src/main/java/com/karrar/movieapp/domain/models/Actor.kt
+++ app/src/main/java/com/karrar/movieapp/domain/models/Actor.kt
@@ -3,5 +3,6 @@ package com.karrar.movieapp.domain.models
 data class Actor(
     val actorID: Int,
     val actorImage: String,
-    val actorName: String
+    val actorName: String,
+    val characterName: String? = "",
 )

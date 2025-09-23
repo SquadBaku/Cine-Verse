a8037abc6ca3ddedfc0b2fb5ac589520bd31bd8d
saraashraf11 <saraashraf1822004@gmail.com>
2025-09-24 01:11:28 +0300

    feature: add onStart in LogoutDialog.kt

--- app/src/main/java/com/karrar/movieapp/ui/profile/logout/LogoutDialog.kt
+++ app/src/main/java/com/karrar/movieapp/ui/profile/logout/LogoutDialog.kt
@@ -3,6 +3,7 @@ package com.karrar.movieapp.ui.profile.logout
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -23,7 +24,6 @@ class LogoutDialog : BaseDialog<DialogLogoutBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dialog?.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
-        setWidthPercent(90)
         collectLast(viewModel.logoutUIEvent) {
             it.getContentIfNotHandled()?.let { onEvent(it) }
         }
@@ -45,5 +45,23 @@ class LogoutDialog : BaseDialog<DialogLogoutBinding>() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.apply {
+            val params = attributes
+            val marginInPx = (16 * resources.displayMetrics.density).toInt()
+            val bottomMarginPx = (30 * resources.displayMetrics.density).toInt()
+
+            params.width = resources.displayMetrics.widthPixels - marginInPx * 2
+            params.height = WindowManager.LayoutParams.WRAP_CONTENT
+            params.gravity = android.view.Gravity.CENTER
+
+            attributes = params
+
+            decorView.setPadding(0, 0, 0, bottomMarginPx)
+        }
+    }
+
 
 }

a4a30687cdcec0820a5a1c24a498343389da3e25
Alaa <alaakhaled9191@gmail.com>
2025-09-27 15:33:35 +0300

    add support for saving and restoring selected bottom navigation item

--- app/src/main/java/com/karrar/movieapp/utilities/PrefsManager.kt
+++ app/src/main/java/com/karrar/movieapp/utilities/PrefsManager.kt
@@ -3,8 +3,10 @@ package com.karrar.movieapp.utilities
 import android.content.Context
 
 object PrefsManager {
-    private const val PREFS_NAME = "app_prefs"
+
+    private const val PREFS_NAME = "app_settings"
     private const val KEY_LANGUAGE = "language"
+    private const val KEY_SELECTED_NAV_ITEM = "selected_nav_item"
 
     fun saveLanguage(context: Context, lang: String) {
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
@@ -14,5 +16,15 @@ object PrefsManager {
     fun getLanguage(context: Context): String {
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         return prefs.getString(KEY_LANGUAGE, "en") ?: "en"
+    }
+
+    fun saveSelectedNavItem(context: Context, itemId: Int) {
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        prefs.edit().putInt(KEY_SELECTED_NAV_ITEM, itemId).apply()
+    }
+
+    fun getSelectedNavItem(context: Context): Int {
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        return prefs.getInt(KEY_SELECTED_NAV_ITEM, 0)
     }
 }
